af16efd6679e8e33b0ca74d11fd9c687433d908b
Raquel Fernandez <rmfernandezbermudez@gmail.com>
2025-03-13 12:42:33 +0100

    Añadido favicon y tittle en pestaña

--- resources/views/components/layouts/layout.blade.php
+++ resources/views/components/layouts/layout.blade.php
@@ -1,12 +1,12 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{$title ?? 'Proyecto Laravel'}}</title>
+    <title>{{$title ?? 'Gatoteca'}}</title>
     @vite("resources/css/app.css", "resources/js/app.js")
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
 </head>
 <body>
 <x-layouts.header />
